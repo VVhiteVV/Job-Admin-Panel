b85bce5dbbe98b5f958be88616213fc66eb6e2b2
VVhiteVV <92418830+VVhiteVV@users.noreply.github.com>
2022-08-15 16:09:01 +0500

    test

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-
-
+//////////Menu open-close
 $('.sideBar-menu__button-hide').click(function (){
    let menu = $('.sideBar-menu');
-   if(menu.hasClass('sideBar-disable')){
+   let item = $('.sideBar-menu__item');
+   let logo = $('.sideBar-menu__logo')
+   if(!menu.hasClass('sideBar-active')){
       menu.removeClass('sideBar-disable').addClass('sideBar-active');
-      this.style.right = "-26px";
+      item.children('a').fadeOut(300);
+      logo.attr('src','../public/logo-s.png');
       this.style.transform = "rotate(180deg)";
+
    }
    else{
       menu.removeClass('sideBar-active').addClass('sideBar-disable');
-      this.style.right = '-13px';
+      item.children('a').fadeIn(300);
+      logo.attr('src','../public/logo.png');
       this.style.transform = "rotate(0deg)"
    }
+
+})
+
+//////////FileName input-file
+$('#input__file').change(function (){
+   let file = this.value.split('\\').length;
+   let fileName = this.value.split('\\');
+   $('.Label-file').text(fileName[file-1]);
 })
